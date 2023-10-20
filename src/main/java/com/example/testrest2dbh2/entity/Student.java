85d2b91119736511cf26bece60ba1d9ecc3f30d9
reffyll <rdm008@yandex.ru>
2023-10-20 14:16:47 +0500

    lr4

---
+++ src/main/java/com/example/testrest2dbh2/entity/Student.java
@@ -0,0 +1,42 @@
+package com.example.testrest2dbh2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "STUDENTS")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "faculty")
+    private String faculty;
+
+    @Column(name = "age")
+    private int age;
+
+    public Student(String name, String surname, String faculty, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.faculty = faculty;
+        this.age = age;
+    }
+
+
+}
